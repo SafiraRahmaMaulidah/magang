0e0975b65a10033e56255b39165c5af6454f6a2e
SafiraRahmaMaulidah <111202214631@mhs.dinus.ac.id>
2025-11-19 21:24:14 +0700

    Initial Commit

---
+++ magang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  magang
+//
+//  Created by MacBook Pro M1 on 19/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ magang/magangApp.swift
@@ -0,0 +1,17 @@
+//
+//  magangApp.swift
+//  magang
+//
+//  Created by MacBook Pro M1 on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct magangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
